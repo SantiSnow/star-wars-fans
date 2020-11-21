e8e36344378aa206d630bf12d6795dff119819c5
Santiago <aguirresantiago81@gmail.com>
2020-11-21 19:53:58 -0300

    Agregado un test que marca el funcionamiento de las consultas preparadas

---
+++ controller/test.php
@@ -0,0 +1,37 @@
+<?php
+
+include 'conexion.php';
+
+$miConexion = new Conexion($host, $db, $usr, $pass);
+
+$recibido = "%balles%";
+
+$stm = $miConexion->getConnection()->prepare("SELECT * FROM armas WHERE Nombre LIKE ?");
+
+$stm->bind_param("s", $recibido);
+
+$stm->execute();
+
+$resultado = $stm->get_result();
+
+$miJson = array();
+
+while($i = $resultado->fetch_assoc()){
+    $miJson[]= array(
+        'Id'=>$i['ID'],
+        'Nombre'=>$i['Nombre'],
+        'Principal_usuario'=>$i['Principal_usuario'],
+        'Tipo'=>$i['Tipo'],
+        'Creador'=>$i['Creador'],
+        'Foto'=>$i['Foto']
+    );
+}
+
+$stringJson = json_encode($miJson);
+
+echo $stringJson;
+
+
+$stm->close();
+$miConexion->closeConnection();
+
