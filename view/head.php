daf961f4b67298cba3e8692aa8c227def9a55164
Santiago Aguirre <aguirresantiago81@gmail.com>
2020-12-16 15:10:40 -0300

    CDN de Bootstrap y JQuery actualizados, parece que sus cdn tuvieron cambios con la actualizacion a Bootstrap 5.0 y los links eran erroneos

--- view/head.php
+++ view/head.php
@@ -4,5 +4,4 @@
 <!-- link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" -->
 <link rel="stylesheet" href="view/css/bootstrap.darkly.min.css">
 <!-- JQuery -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
